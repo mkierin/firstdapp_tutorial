fc7f77e212d74386bf8af012a22d5d3b86b86d31
xtarsy <mkierin@gmail.com>
2018-07-09 23:04:06 +0200

    modified:   build/contracts/Election.json 	modified:   build/contracts/Migrations.json 	modified:   contracts/Election.sol 	new file:   test/election.js

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,35 @@
 pragma solidity 0.4.24;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    //Model a Candidate
+    struct Candidate {
+	uint id;
+	string name;
+	uint voteCount;
 
-    // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
     }
+    //Store Candidates
+    //Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+    //Store Candidates voteCount
+    uint public candidatesCount;
+
+    function Election () public {
+    	addCandidate("Candidate 1");
+
+    	addCandidate("Candidate 2");
+
+    }
+    //name is a localvariable and it is private. No one else add a candidates only contract
+    function addCandidate (string _name) private {
+//increment operator add candidates. CandidatesCount represents the ID of the candidates
+    	candidatesCount ++;
+    	//initiate candidate pass it a name and set it a value of zero. Zero votes
+    	candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+
+    
 }
+
+ 
